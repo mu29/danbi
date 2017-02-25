8dbd167cea218cd49fa8ecd65894c1560f93dfe6
Jubin Park <jubin-park@naver.com>
2017-02-25 19:04:32 +0900

    오리지널 Game.exe로 교체

---
+++ ClientRGSS3/ini.bat
@@ -0,0 +1,10 @@
+@echo off
+copy nul Game.ini
+echo.[Game]>>Game.ini
+echo.Library=RGSS301.dll>>Game.ini
+echo.Scripts=Data\Scripts.rxdata>>Game.ini
+echo.Title=DanbiClient>>Game.ini
+echo.RTP1=Standard>>Game.ini
+echo.RTP2=>>Game.ini
+echo.RTP3=>>Game.ini
+del RGSS104E.dll
